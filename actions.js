b389a64aed159e623576dabe61a569b629787f90
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-08-26 14:26:30 -0400

    Refactor time calculations and improve UI outputs
    Refactored time calculations in actions.js to consistently use minutes for roll change and gap times, and added a function for total run time calculation. Updated index.html to provide default placeholders for output fields and clarified labels for better user understanding. Added error handling for invalid sequence input.

--- actions.js
+++ actions.js
@@ -11,30 +11,48 @@ estimateForm.addEventListener("submit", (e) => {
     calcTotalQuantity();
     calcRollChangeTime();
     calcGapsInRoll();
+    calcTotalRunTime();
 });
 
+// Calcluate total run time: Add production run time, 
+//  roll change time, and gap run time. 
+function calcTotalRunTime() {
+    console.log("total?")
+}
 
 
 // Subtract the sequence numbers to obtain total quantity
 function calcTotalQuantity(){
-    let totalQuantity = seqStart.value - seqEnd.value;
-    document.getElementById('quantity').innerHTML = totalQuantity+ " pieces";
-    return totalQuantity;
+    if (seqStart.value > 0){
+        let totalQuantity = seqStart.value - (seqEnd.value - 1);
+        document.getElementById('quantity').innerHTML = totalQuantity;
+        calcProdRunTime(totalQuantity);
+    } else {
+        console.log("start sequence !> 0");
+        startSequenceError();
+    }  
+}
+
+function calcProdRunTime(totalQuantity){
+    let quantityRunTime = (totalQuantity / prodVelo.value);
+    document.getElementById('qtyRunTime').innerHTML = (Math.round((quantityRunTime + Number.EPSILON) * 100) / 100)+ " Hours";
 }
 
 
 // Calculate total roll change time (total rolls ready * 20 
 // minutes)
 function calcRollChangeTime(){
-    let totalRollChange = (totalReadyRolls.value * 20) / 60;
-    document.getElementById('rollChange').innerHTML = (Math.round((totalRollChange + Number.EPSILON) * 100) / 100)+ " hours";
-    return totalRollChange;
+    let totalRollChange = (totalReadyRolls.value * 20); //In Minutes
+    document.getElementById('rollChange').innerHTML = (Math.round((totalRollChange + Number.EPSILON) * 100) / 100)+ " Minutes";
 }
 
 // Calculate total time needed for gaps in roll (total 
 // number of gaps * 5 minutes)
 function calcGapsInRoll(){
-    let totalGapTime = (numOfGaps.value * 5) / 60;
-    document.getElementById('runGap').innerHTML = (Math.round((totalGapTime + Number.EPSILON) * 100) / 100)+ " hours";
-    return totalGapTime;
+    let totalGapTime = (numOfGaps.value * 5); //In Minutes
+    document.getElementById('runGap').innerHTML = (Math.round((totalGapTime + Number.EPSILON) * 100) / 100)+ " Minutes";
+}
+
+function startSequenceError(){
+    alert("The starting sequence needs to be greater than 0!");
 }
